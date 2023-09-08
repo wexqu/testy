23e05f9594cf6026832128027d0eaa0e9267f17d
wexqu <loganssher2000@gmail.com>
2023-09-07 22:41:43 -0400

    simple flutter word generator app

---
+++ lib/login.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('First Page')),
+      body: Center(child: Text('This is the first page')),
+    );
+  }
+}
---
+++ lib/signup.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class signupPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Second Page')),
+      body: Center(child: Text('This is the second page')),
+    );
+  }
+}
